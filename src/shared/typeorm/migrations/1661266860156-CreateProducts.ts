f8b1dfcea76b248f9b53b55de1e746f7d8ae692c
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-08-23 12:04:47 -0300

    feat: create migration products table

---
+++ src/shared/typeorm/migrations/1661266860156-CreateProducts.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateProducts1661266860156 implements MigrationInterface {
+  name = 'CreateProducts1661266860156';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "products" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" numeric NOT NULL, "quantity" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "products"`);
+  }
+}
